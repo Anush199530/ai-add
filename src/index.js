4d899265448a1082616ce6df177e41929021d31c
Anush199530 <141546279+Anush199530@users.noreply.github.com>
2024-01-21 12:50:14 +0100

    added basics javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecepy(event) {
+  event.preventDefault();
 
+  new Typewriter("#recepy-ingredients", {
+    strings: ["The recepy"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recepyFormElement = document.querySelector("#recepy-generator-form");
+recepyFormElement.addEventListener("submit", generateRecepy);
