67107111cea025ac44c814bee2b7f5534e936ba7
Anush199530 <141546279+Anush199530@users.noreply.github.com>
2024-01-21 13:54:42 +0100

    added the ai api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-function generateRecepy(event) {
-  event.preventDefault();
-
+function displayRecepy(response) {
+  console.log("recepy generateted");
   new Typewriter("#recepy-ingredients", {
-    strings: ["The recepy"],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecepy(event) {
+  event.preventDefault();
+
+  let ingredientsInput = document.querySelector("#user-ingredients");
+  let apiKey = "0t30d9c2bfb348e86oa6585b9339d079";
+  let prompt = `User instructions: generate a recepy with the ingredients given in ${ingredientsInput.value}`;
+  let context =
+    "You are an amazing chef that can cook with everything that is given to him and can come up with amazing and delicious recipies. Weather it is heathy or not, and budged friendly. Your mission is to generate a recipy based on the ingredients given to you and explain how it should be cooked";
+
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log("Generating recepy");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayRecepy);
 }
 
 let recepyFormElement = document.querySelector("#recepy-generator-form");
