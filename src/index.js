4cd3ca45fa148dc8b604f2fd6de353c71b34581d
Anush199530 <141546279+Anush199530@users.noreply.github.com>
2024-01-21 15:37:40 +0100

    improved the user experience

--- src/index.js
+++ src/index.js
@@ -15,9 +15,14 @@ function generateRecepy(event) {
   let apiKey = "0t30d9c2bfb348e86oa6585b9339d079";
   let prompt = `User instructions: generate a recepy with the ingredients given in ${ingredientsInput.value}`;
   let context =
-    "You are an amazing chef that can cook with everything that is given to him and can come up with amazing and delicious recipes. Weather it is heathy or not, and budged friendly. Your mission is to generate a recipe based on the ingredients given to you and explain how it should be cooked. Give a little sturcture to the recipe by making user that each step is under each other.";
+    "You are an amazing chef that can cook with everything that is given to him and can come up with amazing and delicious recipes. Weither it is heathy or not, and budged friendly. Your mission is to generate a recipe based on the ingredients given to you and explain how it should be cooked. The structure of each ingredient and each step has it own line and CAN'T be next to each other.";
 
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let ingredientElement = document.querySelector(".recepy-instruction");
+  ingredientElement.classList.remove("hidden");
+  ingredientElement.innerHTML = `<div class= "generating"> 🧑‍🍳Generating the recipe ..... </div> `;
+
   console.log("Generating recepy");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
